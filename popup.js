b54f95148d714ea85db04a4d24babc234e022bf0
Bianca Ragsdale <128001048+lai-bianca@users.noreply.github.com>
2023-07-09 12:59:33 -0700

    JS updates
    Playing around with the JS. Trying to change the color of the Share elements.

--- popup.js
+++ popup.js
@@ -7,15 +7,32 @@ const tabs = await chrome.tabs.query({
   ]
 });
 
-document.querySelector('ul').append(...elements);
+document.getElementById("shareyour").addEventListener("click", myFunction);
+
+function myFunction() {
+	var elements = document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share"); // get all elements
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "blue";
+	}
+}
+
+//function myFunction(){
+//  document.getElementById("shareyour").style.color = "blue";
+//}
+
+//function myFunction(){
+//  document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share").style.color = "blue";
+//}
+
+// document.querySelector('ul').append(...elements);
 // need to figure out how to create JavaScript to specific buttons. I'm trying to reference "shareyour" below, but I don't think it works. 
-const shareyour = document.querySelector('shareyour');
-shareyour.addEventListener('click', async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  if (tabIds.length) {
-    const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: 'DOCS' });
-  }
-});
+// const shareyour = document.querySelector('shareyour');
+// shareyour.addEventListener('click', async () => {
+//  const tabIds = tabs.map(({ id }) => id);
+//  if (tabIds.length) {
+//    const group = await chrome.tabs.group({ tabIds });
+//    await chrome.tabGroups.update(group, { title: 'DOCS' });
+//  }
+// });
 // logic should be: When you click "share your" in the extension, click the Share element for one unsold listing in your closet. Continue sharing 1 unsold listing at a time, in random order. Should stop when user click "Stop All Shares".
 // Add a per-minute limit. 
