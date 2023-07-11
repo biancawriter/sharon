bb05d93689e273515ac3947f3e93d670f3fac084
Bianca Ragsdale <128001048+lai-bianca@users.noreply.github.com>
2023-07-11 08:10:59 -0700

    Pop-up detection
    Drafted the pop-up detection Chrome API.

--- scripts/content.js
+++ scripts/content.js
@@ -3,6 +3,17 @@
 //    document.body.style.backgroundColor = "red";
 //  }
 //});
+
+// Click "Learn about me" in extension to click "About" on the page.
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === "share-my") {
+var about = document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share");
+for(var i = 0; i < about.length; i++)
+{about[i].click();}
+  }
+});
+
+// Click "Share Your Listings" in extension to click "Share" for the last visible listing on the page.
 chrome.runtime.onMessage.addListener((message) => {
   if (message.type === "about-me") {
 var about = document.getElementsByClassName("btn btn--tertiary tr--capitalize m--l--4");
@@ -11,13 +22,12 @@ for(var i = 0; i < about.length; i++)
   }
 });
 
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "share-my") {
-var about = document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share");
-for(var i = 0; i < about.length; i++)
-{about[i].click();}
-  }
-});
+//Attempting to detect the share pop-up window. Do more research on: https://developer.chrome.com/docs/extensions/reference/windows/#event-onCreated
+chrome.windows.onCreated.addListener(
+  callback: function,
+  filters: "popup",
+)
+
 
 //chrome.runtime.onMessage.addListener((message) => 
 //{
