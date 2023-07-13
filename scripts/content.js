aa22ba3e56c3b19a080475bee7fe374e18314d6e
Bianca Ragsdale <128001048+lai-bianca@users.noreply.github.com>
2023-07-12 21:18:35 -0700

    Try functions
    Tried creating a function for clicking a button and invoking it.

--- scripts/content.js
+++ scripts/content.js
@@ -4,16 +4,9 @@
 //  }
 //});
 
-// Click "Learn about me" in extension to click "About" on the page.
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "share-my") {
-var about = document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share");
-for(var i = 0; i < about.length; i++)
-{about[i].click();}
-  }
-});
 
-// Click "Share Your Listings" in extension to click "Share" for the last visible listing on the page.
+
+// Click "Learn about me" in extension to click "About" on the page.
 chrome.runtime.onMessage.addListener((message) => {
   if (message.type === "about-me") {
 var about = document.getElementsByClassName("btn btn--tertiary tr--capitalize m--l--4");
@@ -22,41 +15,40 @@ for(var i = 0; i < about.length; i++)
   }
 });
 
-// Click "Share to My Followers" in extension to click "Share" for the last visible listing on the page. Attempted to use jquery to get the a element by data-et-name. Doesn't work yet.
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "share-to-followers") {
-  const el1 = document.querySelector('[data-et-name="share_poshmark"]');
-  document.el1.click();
-  }
-});
 
-///
-// Click "Share to My Followers" in extension to click "Share" for the last visible listing on the page.
+// Click "Share Your Listings" in extension to click "Share" for the last visible listing on the page.
+//commenting this out as a test
 //chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "share-to-followers") {
-//var about = document.getElementsByClassName("internal-share__link");
-//for(var i = 0; i < about.length; i++)
-//{about[i].click();}
+//  if (message.type === "share-my") {
+//var listing = document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share");
+//for(var i = 0; i < listing.length; i++)
+//{listing[i].click();}
 //  }
 //});
 
-//Attempting to detect the share pop-up window. Do more research on: https://developer.chrome.com/docs/extensions/reference/windows/#event-onCreated. 
-//Should detect the specific pop-up, and then click the To My Followers icon.
-//chrome.windows.onCreated.addListener(
-//  callback: function,
-//  filters: "popup",
-//)
+// Click "Share to My Followers" in extension to click "Share" for the last visible listing on the page.This finally works! Test if this works when there's a Posh party, bc then there are 2 possible links.
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === "share-to-followers") {
+    document.getElementsByClassName('internal-share__link')[0].click();
+  }
+});
 
+// try using functions to click the share buttons. So far this doesn't work.
+// Click "Share Your Listings" in extension to click "Share" for the last visible listing on the page.
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === "share-my") {
+function shareMy1()
+  }
+});
 
-//chrome.runtime.onMessage.addListener((message) => 
-//{
-////  if (message.type === "change-color") 
-//{
-//	var elements = document.getElementsByClassName("grid-page");
-//	for(var i = 0; i < elements.length; i++)
-//{
-//		elements[i].style.backgroundColor = "red";
-//  }
-//}
-//)
-//};
+function shareMy1() {
+  var listing = document.getElementsByClassName("d--fl ai--c social-action-bar__action social-action-bar__share");
+  for(var i = 0; i < listing.length; i++)
+  {listing[i].click();}
+}
+
+//Next tasks:
+//String together 2 clicks: click share, detect that the pop-up is open, and then click other share.
+//Continue to share multiple listings
+//Set a limit
+//Only share Available listings
