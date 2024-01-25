09d16c193026f4a64c06b9075bfa9fcd21fe421a
biancawriter <ragsdale.bianca@gmail.com>
2024-01-24 16:51:03 -0800

    Rewrote share functions
    Rewrote the share functions to follow my logic. Created a backup of the previous functions--see the "content_THIS_WORKS" file.

--- scripts/content.js
+++ scripts/content.js
@@ -16,31 +16,23 @@ chrome.runtime.onMessage.addListener((message) => {
   }
 });
 
-//Function to click both buttons
-function waitForElement(selector) {//Defines the waitForElement and establishes its parameter as "selector".
-  var element = document.querySelector(selector); //Creates a variable called "element". In theory, it returns the first element on the page that has the "element" attribute. I get lost here, b/c I don't understand what "selector" means in this context. It hasn't been established as am element attribute, so I don't understand how this can retrieve an element. 
-  if (element) { //If the element var exists (so if you're still on the first page?)
-      shareLinkCount--; //Subtracts 1 from the NodeList of all elements with the attribute '.share-gray-large'.
-      element.click(); //Click the element defined by the element variable. 
+var shareLinkCount = document.querySelectorAll('.share-gray-large').length - 1; 
 
-      if (shareLinkCount) { // If the variable is NOT 0 (so if you haven't gone through all the listings yet)
-          setTimeout(clickShareButton, 10000); //Run the clickShareButton function in 10 seconds. This is the 1st share.
-      }
-      else{
-          element.click();
-          clickShareButton(); //not sure if this will work. I replaced the "click finished" with clickShareButton function
-      }
-  } else {//If the element var DOESN'T exist...
-      setTimeout(waitForElement, 2000, selector); //this is the 2nd share
-  }
+function shareToFollowers () {
+  document.querySelector('.internal-share__link').click();
+}
+
+//Function to click both buttons
+function waitForElement() {
+    setTimeout(shareToFollowers, 2000);
+    shareLinkCount--;
+    setTimeout(clickShareButton, 15000);
 }
 
 function clickShareButton() { //Defines how the clickShareButton function works.  
   document.querySelectorAll('.share-gray-large')[shareLinkCount].click(); //First, it creates a NodeList of all elements with the attribute '.share-gray-large'. Then it picks an item from the NodeList based on the # returned by the shareLinkCount variable. Second, it clicks that item.
-  waitForElement('.internal-share__link');//Calls the waitForElement function.
+  waitForElement();//Calls the waitForElement function.
 }
-
-var shareLinkCount = document.querySelectorAll('.share-gray-large').length - 1; //Creates a variable. First, it creates a NodeList of all elements with the attribute '.share-gray-large'. Second, it returns the number of elements in that list by using .length. Third, it subtracts 1 from that number. The final variable is "the total number of elements - 1". Hmm wouldn't that always return the same number?
 
 // Click "Share My Listings" in extension to click "Share" for the last visible listing on the page.
 chrome.runtime.onMessage.addListener((message) => {
